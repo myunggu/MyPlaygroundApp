942af9b4e05319185434fd4f4efe362bc77e81a9
Myung Gu Kim <devmgk@gmail.com>
2024-02-22 16:14:11 -0800

    it sucks ㅠㅠ

--- MyPlaygroundApp/MyPlaygroundApp/ViewController.swift
+++ MyPlaygroundApp/MyPlaygroundApp/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         //Hello full request first
+        
+        
+        let n = 100
     }
 
 
