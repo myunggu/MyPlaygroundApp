0a75166147ff1476f551ff44a7e8cde231398de0
Myung Gu Kim <devmgk@gmail.com>
2024-02-22 16:18:09 -0800

    update App Tests

--- MyPlaygroundApp/MyPlaygroundAppTests/MyPlaygroundAppTests.swift
+++ MyPlaygroundApp/MyPlaygroundAppTests/MyPlaygroundAppTests.swift
@@ -12,6 +12,8 @@ final class MyPlaygroundAppTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        print("Hello World")
     }
 
     override func tearDownWithError() throws {
